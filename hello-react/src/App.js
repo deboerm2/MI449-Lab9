bae86e0d3ee8b661404a752f5dfd5d2b9147961f
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-03-22 19:23:03 +0000

    test that react is working

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (this is a test)
         </a>
       </header>
     </div>
